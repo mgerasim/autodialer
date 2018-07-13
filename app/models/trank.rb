7517c831f9761f51274eb7f2160f8294b5a5f23e
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-07-13 11:00:16 +0400

    Добавить в модель Рабочие каналы проверка наличие полей #10

--- app/models/trank.rb
+++ app/models/trank.rb
@@ -1,2 +1,6 @@
 class Trank < ApplicationRecord
+    validates :name, presence: true
+    validates :waittime, presence: true
+    validates :callerid, presence: true
+    validates :callcount, presence: true
 end
