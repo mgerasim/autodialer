7ab06acf1193912c6a737e8ccaee23b731737b47
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-11-06 16:18:15 +0300

    Добавлено функция создания тестового звонка в модель транка

--- app/models/trank.rb
+++ app/models/trank.rb
@@ -3,7 +3,23 @@ class Trank < ApplicationRecord
     validates :waittime, presence: true
     validates :callerid, presence: true
     validates :callcount, presence: true
-    
-    def check
+   
+    def check(telephone)
+      setting = Setting.first
+      puts telephone
+      File.open(Dir::Tmpname.create(['tmp_' + telephone + "_#{self.name}_", '.call']) { }.to_s, "w+") do |f|
+                    f.chmod(0666)
+    	            f.puts("Channel: SIP/" + telephone +  "@#{self.name}")
+                    f.puts("Callerid: " + self.callerid)
+	            f.puts("MaxRetries: 0")
+                    f.puts("RetryTime: 20")
+                    f.puts("WaitTime: " + self.waittime.to_s)
+                    f.puts("Context: from-trunk")
+                    f.puts("Extension: s")
+                    f.puts("Priority: 1")
+		    FileUtils.mv(f.path, setting.outgoing + '/' + File.basename(f.path))     
+      end
+     
+
     end
 end
