be417bfe52c3baf5142f5839a49375afc4c74aab
mgerasim <mgerasim@inbox.ru>
2018-12-17 09:42:19 +1000

    Update INSTALL_RAILS_centos.sh

--- INSTALL_RAILS_centos.sh
+++ INSTALL_RAILS_centos.sh
@@ -1,7 +1,27 @@
-sudo apt update
-sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+sudo yum install -y git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel wget libcurl-devel.x86_64
+sudo yum install -y crontabs cronie cronie-anacron
+
+#Add the EPEL yum repository. 
+yum -y install epel-release
+
+# Nginx
+sudo yum install -y nginx 
+systemctl start nginx
+sudo systemctl enable nginx
+
+#Then install the Node.js package:
+yum install -y nodejs
+
+# Install a Database MariaDB \ MySQL
+sudo yum install -y mariadb-server mariadb-devel
+sudo systemctl enable mariadb
+systemctl start mariadb
 
 useradd rails
+echo 'kxJFqz' | passwd rails --stdin
+usermod -aG wheel rails 
+
+su - rails
 
 
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+
