25f400e483e63f289c2aaa95d273a63d6203f9a8
mgerasim <mgerasim@inbox.ru>
2018-12-17 08:46:02 +1000

    Create INSTALL_RAILS_centos.sh

---
+++ INSTALL_RAILS_centos.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+
+useradd rails
+
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
