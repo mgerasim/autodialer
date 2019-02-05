33618e22652886c5d793c43eba5a9feb20bc479d
mgerasim <mgerasim@inbox.ru>
2019-02-05 14:04:40 +1000

    Update outgoing.rb

--- app/models/outgoing.rb
+++ app/models/outgoing.rb
@@ -21,6 +21,47 @@ class Outgoing < ApplicationRecord
 		""
 	end
     end
+	
+    def total_count
+        date = DateTime.now
+                Outgoing.where(:updated_at => (date.beginning_of_day..date.end_of_day)).count
+       # Outgoing.where(:updated_at => (date.beginning_of_day..date.end_of_day))
+ #      .where(:status => ["DIALED", "ANSWERED", "NO ANSWER", "FAILED", "BUSY"]).count
+    end
+
+    def answer_total_count
+        date = DateTime.now
+        Answer.where(:updated_at => (date.beginning_of_day..date.end_of_day)).count
+    end
+
+    def outgoing_precent
+        outgoing_count = self.total_count
+
+       if outgoing_count == 0
+           0
+       else
+            ((self.answer_total_count.to_f / total_count.to_f) * 100).round(2)
+       end
+    end
+
+
+    def outgoing_answer_total_count
+        date = DateTime.now
+       Outgoing.where(:updated_at => (date.beginning_of_day..date.end_of_day))
+        .where(:status => ["ANSWERED"]).count
+    end
+
+    def outgoing_answer_precent
+        outgoing_answer_count = self.outgoing_answer_total_count
+
+        if outgoing_answer_count == 0
+            0
+        else
+            ((answer_total_count.to_f / outgoing_answer_total_count.to_f) * 100).round(2)
+        end
+    end
+
+	
 
     def google_sheet_save(title)
       begin
