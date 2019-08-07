c2e356576408cb492d5644b06d47d08eefd57c8d
Mikhail Gerasimov <mgerasim@inbox.ru>
2019-08-07 23:41:02 +0300

    Провка скрипта вставки отклика

--- asterisk/outgoing_status.sh
+++ asterisk/outgoing_status.sh
@@ -2,5 +2,6 @@
 
 AUTODIALER_ROOT="/home/rails/projects/autodialer"
 
-/bin/mysql --user=avtodialer --password=avtodialer avtodialerdb_prod -e 'INSERT INTO answers(created_at, updated_at, contact, level) VALUES(NOW(), NOW(), '$1', '$2')' >> $AUTODIALER_ROOT/log/outgoing_status.log 2>> $AUTODIALER_ROOT/log/outgoing_status_error.log
+/bin/mysql --user=avtodialer --password=avtodialer avtodialerdb_prod -e 'INSERT INTO answers(created_at, updated_at, contact, level) VALUES(NOW(), NOW(), '$1', '$2') ON DUPLICATE KEY UPDATE created_at=NOW(), level='$2''  >> $AUTODIALER_ROOT/log/outgoing_status.log 2>> $AUTODIALER_ROOT/log/outgoing_status_error.log
+
 
