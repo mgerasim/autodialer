ed13524480a1747c1e7192bf567f0c9f8379843e
Mikhail Gerasimov <mgerasim@inbox.ru>
2019-08-10 14:01:26 +0300

    Привязка транка к отклику

--- asterisk/outgoing_status.sh
+++ asterisk/outgoing_status.sh
@@ -2,6 +2,5 @@
 
 AUTODIALER_ROOT="/home/rails/projects/autodialer"
 
-/bin/mysql --user=avtodialer --password=avtodialer avtodialerdb_prod -e 'INSERT INTO answers(created_at, updated_at, contact, level) VALUES(NOW(), NOW(), '$1', '$2') ON DUPLICATE KEY UPDATE created_at=NOW(), level='$2''  >> $AUTODIALER_ROOT/log/outgoing_status.log 2>> $AUTODIALER_ROOT/log/outgoing_status_error.log
-
+/bin/mysql --user=avtodialer --password=avtodialer avtodialerdb_prod -e 'INSERT INTO answers(created_at, updated_at, contact, level, trank_id) VALUES(NOW(), NOW(), '$1', '$2', (SELECT id FROM tranks WHERE name='$3' LIMIT 1)) ON DUPLICATE KEY UPDATE created_at=NOW(), level='$2', trank_id=((SELECT id FROM tranks WHERE name='$3' LIMIT 1))'
 
