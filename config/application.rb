5131fb546429e8a0b7eb50de369c810d4f3a1b64
Mikhail Gerasimov <Mikhail Gerasimov>
2019-09-09 14:32:54 +0300

    cors

--- config/application.rb
+++ config/application.rb
@@ -16,17 +16,14 @@ module Avtodailer
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Moscow'
     config.active_record.default_timezone = :local
- 	
-    config.middleware.insert_before 0, "Rack::Cors" do
+
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
-    end
+    end 
+	
 
   end
 end
