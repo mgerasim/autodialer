b272bddd2d384bf9bdd83db845320b7f18dab521
Mikhail Gerasimov <Mikhail Gerasimov>
2019-09-09 12:28:39 +0300

    cors

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -20,6 +20,12 @@ class AnswersController < ApplicationController
     @outgoing_answer_precent = ((@answer_total.count.to_f / @outgoing_answer_total.count.to_f) * 100).round(2) if @outgoing_answer_total.count > 0
 
     @answers = Answer.where.not(:level => 0)
+
+headers['Access-Control-Allow-Origin'] = '*'
+headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+headers['Access-Control-Request-Method'] = '*'
+headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
     respond_to do |format|
         format.html
         format.csv { send_data @answers.to_csv, filename: "answers-#{Date.today}.csv" }
