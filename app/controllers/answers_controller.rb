7ec44d9d438984ab1b04aeb1b5ca12d1ed75c05b
Mikhail Gerasimov <Mikhail Gerasimov>
2019-09-09 14:40:01 +0300

    answers index skip

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,8 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
+   skip_before_action :require_login, :only => [:index]
+
   # GET /answers
   # GET /answers.json
   def index
