4b4667d9677765f057a836fb8166a1c578818fb2
Mikhail Gerasimov <mgerasim@inbox.ru>
2019-09-10 15:22:24 +0300

    outgoing json

--- app/controllers/outgoings_controller.rb
+++ app/controllers/outgoings_controller.rb
@@ -11,7 +11,7 @@ class OutgoingsController < ApplicationController
  	format.html
 #	format.csv { send_data Outgoing.where("EXISTS (select * from answers where INSTR(telephone, contact) = 0)").to_csv, filename: "autodialer-#{Date.today}.csv" }    
   format.csv { send_data Outgoing.all.to_csv, filename: "autodialer-#{Date.today}.csv" } 
-  format.json { render :json => Outgoing.all, :except => [:created_at, :updated_at]}
+  format.json { render :json => Outgoing.where.not(:trank => nil), :except => [:created_at, :updated_at]}
     end
   end
 
