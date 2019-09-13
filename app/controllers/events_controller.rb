45413ba1f451a7649e530ccb3c3e04e85f41bdb0
Mikhail Gerasimov <mgerasim@inbox.ru>
2019-09-13 07:06:39 +0300

    telephone-call-events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,26 @@
+require 'json'
 class EventsController < ApplicationController
   skip_before_action :require_login, :only => [:call, :summary, :dtfm]
+  skip_before_action :verify_authenticity_token
+
   def call
-    logger.debug(params)
     
+    logger.debug(params)
+
+    json = JSON.parse(params)
+
+    if (json["call_state"] == 'Appeared') 
+
+      telephone = json["from"]["number"]
+
+      logger.debug(telephone)
+
+      telephone = telephone.split(//).last(10).to_s
+
+      logger.debug(telephone)
+
+    end
+
   end
 
   def dtmf
