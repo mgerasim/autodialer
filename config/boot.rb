95435fa50e9628d0a6896672e55e2dc2d2b17708
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-07-05 13:19:23 +0300

    Добавил спецефичный порт

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'rails/commands/server'
 
 module Rails
   class Server
